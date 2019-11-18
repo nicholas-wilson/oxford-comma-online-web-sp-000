5fd6d4fd05e98043e18fd82ace8461ca1ccac432
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 22:36:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
   string_with_oxford_comma = ""
+  if (array.size == 2)
+    return "#{array[0]} and #{array[1]}."
+  end
   array.each_index do |current_index|
     if (current_index == array.size - 1)
       string_with_oxford_comma << "and #{array[current_index]}"
